7e0b691d7e903a3cf721dd058b03d33902f503bd
kdtahersnowflake <131980356+kdtahersnowflake@users.noreply.github.com>
2023-04-27 20:20:48 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -10,3 +10,4 @@ streamlit.text('Omlette')
 streamlit.header('Build your own fruit smoothie')
 
 import pandas
+my_fruit_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
