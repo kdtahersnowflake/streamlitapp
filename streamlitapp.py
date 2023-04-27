9eeb430c49fa3d51c80199f9a7caa1050bfa357d
kdtahersnowflake <131980356+kdtahersnowflake@users.noreply.github.com>
2023-04-27 19:59:38 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -3,8 +3,3 @@ import streamlit
 streamlit.title('My parents new healthy Dinner')
 
 Streamlit.header('Breakfast Menu')
-
-
-Streamlit.text('Omlette')
-Streamlit.text('Tea')
-Streamlit.text('Boiled eggs')
