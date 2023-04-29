55ef88fa4e8efc3241c39868591bfea34bfb3357
kdtahersnowflake <131980356+kdtahersnowflake@users.noreply.github.com>
2023-04-29 17:46:31 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -8,7 +8,7 @@ my_cur.execute("SELECT CURRENT_ROLE()")
 
 my_cur.execute("SELECT * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
 
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
